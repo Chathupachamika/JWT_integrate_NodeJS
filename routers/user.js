7606fbb1aad68eadf2533d97abe48aa57cca9676
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-14 12:07:30 +0530

    morning14 commit

--- routers/user.js
+++ routers/user.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
 
 const router = express.Router();
 
-router.prototype('/login',(req,res)=>{
-    res.send("User is logged in");
+router.post('/login', (req, res) => {
+    const { username } = req.body;
+
+    if (!username) {
+        return res.status(400).send({ error: 'Username is required' });
+    }
+
+    const user = { name: username };
+
+    const token = jwt.sign(user, process.env.TOKEN_KEY);
+    res.send({ token });
 });
 
 module.exports = router;
