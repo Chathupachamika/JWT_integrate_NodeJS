8c91361ad6c18357bae7242448a3b9f6d47cd416
Chathupa Chamika <168677571+Chathupachamika@users.noreply.github.com>
2025-01-14 20:53:36 +0530

    update commit

--- app.js
+++ app.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const user = require('./routers/user');
-const student = require('./routers/student');
+const connectDB = require('./db'); // Import the DB connection
+const cors = require('cors'); // Import the CORS middleware
+require('dotenv').config();
 
 const app = express();
 
+// Connect to MongoDB
+connectDB();
+
+// Enable CORS for all origins (you can restrict it to specific origins if needed)
+app.use(cors());
+
+// Middleware to parse JSON
 app.use(express.json());
 
-app.use('/api/user', user);
-app.use('/api/student', student);
+// Routes
+const userRoutes = require('./routers/user');
+const studentRoutes = require('./routers/student');
+const auth = require('./middleware/auth');
 
+app.use('/api/user', userRoutes);
+app.use('/api/student', auth, studentRoutes);
+
+// Start the server
 const port = process.env.PORT || 3003;
-
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
